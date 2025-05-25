c487dcddd244aaf248014fbad28fb7381a97d987
aayushrajverma <115358980+Aayush-05@users.noreply.github.com>
2025-05-25 21:20:37 +0530

    Add files via upload

---
+++ 2131. Longest Palindrome.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int longestPalindrome(vector<string>& words) {
+        unordered_map<string, int> freq;
+        int length = 0;
+        for (const string& word : words) {
+            string rev = word;
+            reverse(rev.begin(), rev.end());
+
+            if (freq[rev] > 0){
+                length += 4;
+                freq[rev]--;
+            } else {
+                freq[word]++;
+            }
+        }
+        for (auto& [word, count] : freq) {
+            if (word[0] == word[1] && count > 0) {
+                length += 2;
+                break;
+            }
+        }
+
+        return length;
+    }
+};
